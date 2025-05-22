10bf0d785b4e875082c2bffa4f495431e8493a84
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2025-05-22 18:52:25 -0300

    install qrcode canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
+import {QRCodeCanvas} from 'qrcode.react';
 
 
 export default function Home() {
@@ -10,6 +11,34 @@ export default function Home() {
           <h1 className='text-5xl text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-white to-[#3F81E8]'>OLÁ, SEJA BEM VINDO! </h1>
               <span className="text-3xl mt-20 font-extrabold text-white text-center">Crie QR codes dinâmicos, ilimitados e de forma prática.</span>
                 <div className="bg-white w-auto h-auto mt-10 rounded-xl">
+                    <input
+                      type="text"
+                      placeholder="Digite seu link"
+                      className="bg-black"
+                      
+                      />
+                      <p>
+                        Pré visualização QR Code
+                      </p>
+
+                      <QRCodeCanvas
+                          value={"https://picturesofpeoplescanningqrcodes.tumblr.com/"}
+                          title={"Title for my QR Code"}
+                          size={128}
+                          bgColor={"#ffffff"}
+                          fgColor={"#000000"}
+                          level={"L"}
+                          imageSettings={{
+                            src: "https://static.zpao.com/favicon.png",
+                            x: undefined,
+                            y: undefined,
+                            height: 24,
+                            width: 24,
+                            opacity: 1,
+                            excavate: true,
+                          }}
+                        />
+
                   <div className="flex flex-col">
 
                   </div>
